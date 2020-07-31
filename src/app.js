0d55bfc7a3370256371b987841e15a063af6d1a9
Karthick M <mkarthickakmn@gmail.com>
2020-07-31 19:06:28 +0530

    set for heroku

--- src/app.js
+++ src/app.js
@@ -1,12 +1,13 @@
 const express=require("express");
 const path=require("path");
 const app=express();
+const port=process.env.PORT||3000;
 var directory=path.join(__dirname,'../public');
 app.use(express.static(directory));
 
 app.get("",(req,res)=>{
 	res.send(index);
 })
-app.listen(3000,()=>{
-	console.log("listen to port 3000");
+app.listen(port,()=>{
+	console.log("listen to port "+port);
 })
